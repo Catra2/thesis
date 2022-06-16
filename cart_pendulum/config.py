dc14ede5aa5562940f4d0951a0be21cc0657cfd1
alex <alexandertmeyer@gmail.com>
2022-06-16 19:09:18 +0900

    create config

---
+++ cart_pendulum/config.py
@@ -0,0 +1,18 @@
+"""
+Configurable parameters
+"""
+
+"""
+File structure
+"""
+base_path = '/'
+
+"""
+AI-related parameters
+"""
+learning_rate = 1e-3
+
+"""
+MPC-related parameters
+"""
+horizon = 20
